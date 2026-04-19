f941b8159b02bf61be53ac0a9079775630ec5314
Michal Fukowski <michal01f@gmail.com>
2026-04-19 19:52:31 +0200

    added code written during lesson

---
+++ SimpleWebApplication/SimpleWebApplication/Controllers/RoomController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleWebApplication.DTOs;
+using SimpleWebApplication.Models;
+
+namespace SimpleWebApplication.Controllers;
+
+[Route("api/rooms")]
+[ApiController]
+public class RoomController : ControllerBase
+{
+    public static List<Rooms> rooms = new List<Rooms>()
+    {
+        new Rooms() { Id = 1, Name = "Room 1", Capacity = 5 },
+        new Rooms() { Id = 2, Name = "Room 2", Capacity = 3 },
+        new Rooms() { Id = 3, Name = "Room 3", Capacity = 7 }
+    };
+
+    // GET api/rooms
+    [HttpGet]
+    public IActionResult Get([FromQuery] int? minCapacity = 0)
+    {
+        return Ok(rooms.Where(x => x.Capacity >= minCapacity));
+    }
+
+    // GET /api/rooms/{id}
+    // GET /api/rooms/3
+    [Route("{id}")]
+    [HttpGet]
+    public IActionResult GetById(int id)
+    {
+        var room = rooms.FirstOrDefault(x => x.Id == id);
+
+        if (room == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(room);
+    }
+
+    // POST api/rooms { "Name" : "Room1", "Capacity" : 10}
+    [HttpPost]
+    public IActionResult Post([FromBody] CreateRoomDto createRoomDto)
+    {
+        var room = new Rooms()
+        {
+            Id = rooms.Count + 1,
+            Name = createRoomDto.Name,
+            Capacity = createRoomDto.Capacity
+        };
+
+        rooms.Add(room);
+
+        return CreatedAtAction(nameof(GetById), new { id = room.Id }, room);
+    }
+}
---
+++ SimpleWebApplication/SimpleWebApplication/Models/Rooms.cs
@@ -0,0 +1,8 @@
+﻿namespace SimpleWebApplication.Models;
+
+public class Rooms
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Capacity { get; set; }
+}
